eb658a74ba3ae9b63ac0ff9c17e1f3fe11b8d13d
Codesworth <codezworth@gmail.com>
2019-04-20 21:09:01 +0000

    FInished Subscriptions

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,18 +1,40 @@
 const Subscription = {
   comment: {
-    subscribe(parent, { postId }, { pubsub, db }, info) {
-      const post = db.posts.find(x => x.id === postId && x.published === true);
-      if (!post) {
-        throw new Error("No Post found");
-      }
+    subscribe(parent, { postId }, { prisma }, info) {
+      return prisma.subscription.comment(
+        {
+          where: {
+            node: {
+              post: {
+                id: postId
+              }
+            }
+          }
+        },
+        info
+      );
+      //   const post = db.posts.find(x => x.id === postId && x.published === true);
+      //   if (!post) {
+      //     throw new Error("No Post found");
+      //   }
 
-      return pubsub.asyncIterator(`comment ${postId}`);
+      //   return pubsub.asyncIterator(`comment ${postId}`);
+      //
     }
   },
 
   post: {
-    subscribe(parent, args, { pubsub }, info) {
-      return pubsub.asyncIterator("post");
+    subscribe(parent, args, { prisma }, info) {
+      return prisma.subscription.post(
+        {
+          where: {
+            node: {
+              published: true
+            }
+          }
+        },
+        info
+      );
     }
   }
 };
